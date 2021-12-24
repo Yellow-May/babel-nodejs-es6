c5a001c4ce9b98b734e7e3fc3a34dc57e948b942
Yellow May <kwerep09@yahoo.com>
2021-12-24 07:11:04 +0100

    👌 IMPROVE: env.PORT

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import express from 'express';
-import { exit } from 'process';
+import { exit, env } from 'process';
 
 const app = express();
 
@@ -9,7 +9,7 @@ app.get('/', (_req, res) => res.send('Hello Server'));
 
 const start = async () => {
 	try {
-		app.listen(7543, () => console.log('Server listening'));
+		app.listen(env.PORT || 7543, () => console.log('Server listening'));
 	} catch (error) {
 		console.log(error);
 		exit(1);
