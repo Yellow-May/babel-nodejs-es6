ef6cfa885a02b83db8b695bb2a78d4f4ee97bbc0
Yellow May <kwerep09@yahoo.com>
2021-12-24 11:02:09 +0100

    👌 IMPROVE: port change

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.get('/', (_req, res) => res.send('Hello Server'));
 
 const start = async () => {
 	try {
-		app.listen(7543 || env.PORT, () => console.log('Server listening'));
+		app.listen(env.PORT || 7543, () => console.log('Server listening'));
 	} catch (error) {
 		console.log(error);
 		exit(1);
