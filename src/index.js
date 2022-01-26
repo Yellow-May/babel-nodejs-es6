913d1ec70de8be06c6a62940748724267017e499
Yellow May <kwerep09@yahoo.com>
2022-01-26 19:14:25 +0100

    🐛 FIX: PORT error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import express from 'express'
-import { exit } from 'process'
+import { exit, env } from 'process'
 import config from 'config'
 
 const app = express()
@@ -9,7 +9,9 @@ app.get('/', (_req, res) => res.send('Hello Server'))
 
 const start = async () => {
    try {
-      app.listen(config.get('port'), () => console.warn('Server listening'))
+      app.listen(env.PORT || config.get('port'), () =>
+         console.warn('Server listening')
+      )
    } catch (error) {
       console.warn(error)
       exit(1)
